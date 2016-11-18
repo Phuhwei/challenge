92316eb01b56470f73500b1ff331e487344651b2
PhuHui <lenardlian@gmail.com>
2016-11-18 04:16:13 -0500

    Second commit

---
+++ REST.js
@@ -0,0 +1,51 @@
+var mysql   = require("mysql");
+
+function REST_ROUTER(router,connection) {
+    var self = this;
+    self.handleRoutes(router,connection);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection) {
+    var self = this;
+    router.get("/",function(req,res){
+        res.json({"Message" : "Hello World !"});
+    });
+    router.get("/users",function(req,res){
+        var query = "SELECT * FROM ??";
+        var table = ["users"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+    router.get("/users/:id",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["users","id",req.params.id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+    router.post("/users",function(req,res){
+        var query = "INSERT INTO ??(??,??) VALUES (?,?)";
+        var table = ["users","username","email",req.body.username,req.body.email];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Added !"});
+            }
+        });
+    });
+}
+
+module.exports = REST_ROUTER;
